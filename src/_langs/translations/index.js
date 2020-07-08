b3e5ee174cdeb42f9adb9ed84172383eae5e073a
Alexey Skrypnik <unrealvsrealer@gmail.com>
2020-07-08 12:18:30 +0300

    Extra translations added for testing.

--- src/_langs/translations/index.js
+++ src/_langs/translations/index.js
@@ -1,21 +1,31 @@
-import en from './default'
-import ru from './ru'
-import uk from './uk'
-import it from './it'
-import es from './es'
+import en from './default';
+import ru from './ru';
+import uk from './uk';
+import it from './it';
+import es from './es';
+import fr from './fr';
+import hu from './hu';
+import zh from './zh';
+import de from './de';
+import he from './he';
 
 import overrides_all from './overrides/all';
 import {mergeDeep} from "sickspack/deep-merge";
 
 const applyOverrides = (lang, custom = {}) =>
 {
-	return mergeDeep(lang, overrides_all, custom);
+	return mergeDeep({}, en, lang, overrides_all, custom);
 };
 
 export default {
-	en: applyOverrides(en),
+	en: en,
 	ru: applyOverrides(ru),
 	uk: applyOverrides(uk),
 	it: applyOverrides(it),
 	es: applyOverrides(es),
+	fr: applyOverrides(fr),
+	hu: applyOverrides(hu),
+	zh: applyOverrides(zh),
+	de: applyOverrides(de),
+	he: applyOverrides(he),
 }
